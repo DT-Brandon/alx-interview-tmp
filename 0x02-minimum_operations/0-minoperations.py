831e65c669091361fd0688787964085b3c115a23
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-08-07 23:00:13 +0000

    task finished

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    provides a function that returns the minimum number of operations
+    to reach a number of  n H
+"""
+
+def minOperations(n):
+    """
+    Calculate the minimum number of operations to reduce a number to 1.
+
+    Args:
+        n (int): The number to be reduced.
+
+    Returns:
+        int: The total number of operations.
+    """
+
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+
+    return operations
