c55a0426d2f7511a0d524948d6e68eaaaa3dc8b1
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-09-13 13:54:16 +0000

    pycodestyle fixes

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,23 +4,24 @@
 This module contains a function to rotate a 2D matrix.
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a given 2D matrix.
-    
+
     Args:
         matrix (list): The 2D matrix to rotate.
-    
+
     Returns:
         None. The matrix is rotated in-place.
     """
     n = len(matrix)
-    
+
     # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Reverse each row of the transposed matrix
     for i in range(n):
         matrix[i] = matrix[i][::-1]
